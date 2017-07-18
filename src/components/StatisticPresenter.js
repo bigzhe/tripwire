3caa69d2ca6bffc791dbb182040c8390568c958e
Haozhe <imhaozhe@gmail.com>
2017-07-18 13:37:12 +0100

    nothing

---
+++ src/components/StatisticPresenter.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Divider, Container, List, Header, Menu} from 'semantic-ui-react'
+import UserPresenter from './UserPresenter'
+import StatePresenter from './StatePresenter'
+
+const ModelPresenter = ({model, presentFilter, dispatchSetPresentFilter, attackPattern}) => {
+    
+  return (
+    <Container>
+      <Header >
+        Model Presenter
+      </Header>
+      <Menu tabular>
+        <Menu.Item name='User' active={presentFilter.showType === 'UserView'} onClick={() => dispatchSetPresentFilter('UserView')} />
+        <Menu.Item name='Node' active={presentFilter.showType === 'StateView'} onClick={() => dispatchSetPresentFilter('StateView')} />
+      </Menu>
+
+      {
+      (model !== 'Loading' && attackPattern !== 'Loading') ?
+        presentFilter.showType === 'UserView' ?
+          <Container>
+            {
+              Object.entries(model.UserView).map(([user,positions]) => 
+                <UserPresenter key={user} {...{user,positions}} />
+              )
+            }
+          </Container>
+        :
+          <Container>
+            <StatePresenter key={id} {...{id,users,patterns: attackPattern.states, Track: model.Track}} />
+          </Container>
+        
+        :
+        <Header>Loading</Header>
+      }
+    </Container>
+  )
+}
+export default ModelPresenter
