af58720899661c96b83f909cf74936cc5b82d1c5
Haozhe <psyhz2@nottingham.ac.uk>
2017-07-27 15:25:41 +0100

    package

---
+++ node_modules/react-d3-graph/lib/components/Graph/const.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    COORDS_SEPARATOR: ',',
+    FORCE_IDEAL_STRENGTH: -100,
+    FORCE_X: 0.06,
+    FORCE_Y: 0.06,
+    GRAPH_CONTAINER_ID: 'graph-container-zoomable',
+    GRAPH_WRAPPER_ID: 'graph-wrapper',
+    KEYWORDS: {
+        SAME: 'SAME'
+    },
+    LINK_CLASS_NAME: 'link',
+    LINK_IDEAL_DISTANCE: 100, // @todo: Expose as configurable,
+    NODE_CLASS_NAME: 'node',
+    SYMBOLS: {
+        CIRCLE: 'circle',
+        CROSS: 'cross',
+        DIAMOND: 'diamond',
+        SQUARE: 'square',
+        STAR: 'star',
+        TRIANGLE: 'triangle',
+        WYE: 'wye'
+    }
+};
---
+++ node_modules/react-d3-graph/lib/err.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    GRAPH_NO_ID_PROP: 'id prop not defined! id property is mandatory and it should be unique.'
+};
